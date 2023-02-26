49b26348fd24e8d681014796c7dd6f9dabcc2a8a
Vanessa Garcia <112432406+VanessaGVilla@users.noreply.github.com>
2023-02-25 23:34:26 -0800

    Add project files.

---
+++ COMP003A.Assignment6/Program.cs
@@ -0,0 +1,16 @@
+﻿/*
+    * Author: Vanessa Garcia
+    * Course: COMP-003A
+    * Purpose: Methods and XML Comments 5
+    */
+
+namespace COMP003A.Assignment6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
